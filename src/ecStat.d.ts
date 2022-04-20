5acedd7250c8dc713afdfcec8215a5a971f106d7
Go Taku <b100dy-r488it@outlook.com>
2022-04-20 11:19:34 +0900

    fix react import

--- src/ecStat.d.ts
+++ src/ecStat.d.ts
@@ -48,10 +48,19 @@ declare namespace EChartsStat {
 
 }
 
-declare module 'echarts-stat' {
-	export = EChartsStat
-}
+import type { ExternalDataTransform } from "@manufac/echarts-simple-transform";
 
+/**
+ * Needed because of: https://github.com/ecomfe/echarts-stat/issues/35
+ * Module augmentation: https://www.typescriptlang.org/docs/handbook/declaration-merging.html#module-augmentation
+ */
+declare module "echarts-stat" {
+  let transform: {
+    regression: ExternalDataTransform;
+    histogram: ExternalDataTransform;
+    clustering: ExternalDataTransform;
+  };
+}
 
 
 
