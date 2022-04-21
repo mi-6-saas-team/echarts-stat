bf06a5b8b7bb9313d68c86ba891ac3bec7e2c448
Go Taku <b100dy-r488it@outlook.com>
2022-04-21 09:53:16 +0900

    fix ts types

--- src/ecStat.d.ts
+++ src/ecStat.d.ts
@@ -46,21 +46,19 @@ declare namespace EChartsStat {
 		function sum(data: Array<number>): number
 	}
 
+	type Transform = {
+		type: string;
+		transform: any;
+	}
+	
+	let transform: {
+		regression: Transform
+		histogram: Transform
+		clustering: Transform
+	};
+
 }
 
-import type { ExternalDataTransform } from "@manufac/echarts-simple-transform";
-
-/**
- * Needed because of: https://github.com/ecomfe/echarts-stat/issues/35
- * Module augmentation: https://www.typescriptlang.org/docs/handbook/declaration-merging.html#module-augmentation
- */
-declare module "echarts-stat" {
-  let transform: {
-    regression: ExternalDataTransform;
-    histogram: ExternalDataTransform;
-    clustering: ExternalDataTransform;
-  };
+declare module 'echarts-stat' {
+	export = EChartsStat
 }
-
-
-
